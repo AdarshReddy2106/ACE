3ffad61c25e57ce94b97968ba6364e9557c349a5
Harshitha <harshithabeere06@gmail.com>
2025-07-16 14:27:54 +0530

    Made slideshow automatic

--- js/ImageSlider.js
+++ js/ImageSlider.js
@@ -1,5 +1,21 @@
 let slideIndex = 1;
+let slideInterval; // For automatic slideshow
 showSlides(slideIndex);
+
+// Start automatic slideshow
+function startSlideShow() {
+  slideInterval = setInterval(() => {
+    plusSlides(1);
+  }, 3000); // Change slide every 3 seconds
+}
+
+// Stop automatic slideshow
+function stopSlideShow() {
+  clearInterval(slideInterval);
+}
+
+// Start slideshow on page load
+startSlideShow();
 
 // Next/previous controls
 function plusSlides(n) {
